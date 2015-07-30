80b89f079650dba364a96de4d9d48f773c583611
ralam <rehan.alam@gmail.com>
2015-07-29 17:17:26 -0700

    Add snake movement

--- snake/js/snake.js
+++ snake/js/snake.js
@@ -6,6 +6,17 @@
     this.segments = [ new Game.Coord([0,0], [1,0])  ];
   }
 
+  Snake.prototype.move = function () {
+    this.segments.map( function(segment) {
+      segment[0] + dir[0];
+      segment[1] + dir[1];
+    });
+  };
+
+  Snake.prototype.turn = function (newDirection) {
+    this.dir = newDirection;
+  };
+
 
 
 })();
